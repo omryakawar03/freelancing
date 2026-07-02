d4d23393bf865f1eb4e5029b176eed01694928e7
omryakawar <omryakawar@gmail.com>
2026-07-02 11:58:56 +0530

    add

--- terraform/main.tf
+++ terraform/main.tf
@@ -5,16 +5,24 @@ provider "aws" {
 data "aws_vpc" "default" {
   default = true
 }
-data "aws_subnet_ids" "default" {
-  vpc_id = data.aws_vpc.default.id
+
+data "aws_subnets" "default" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_vpc.default.id]
+  }
 }
 
 module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
-  cluster_name    = "zero-downtime-cluster"
-  vpc_id     = data.aws_vpc.default.id
-  subnet_ids = data.aws_subnet_ids.default.ids
+
+  cluster_name = "zero-downtime-cluster"
+  vpc_id       = data.aws_vpc.default.id
+  
+  
+  subnet_ids   = data.aws_subnets.default.ids
+
   eks_managed_node_groups = {
     default = {
       min_size       = 1
